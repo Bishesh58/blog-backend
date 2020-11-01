33b3f48babc56e4b79a5917d0793e50554ed236c
Bishesh <bishesh.sunam@gmail.com>
2020-11-01 22:09:55 +1300

    completed image upload to json

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+var multer = require('multer');
 const Post = require("./api/models/posts");
-
+const postData = new Post();
 
 const app = express();
 app.use(cors());
 app.use(express.json())
 app.use('/assets', express.static('assets'))
-const postData = new Post();
 
 
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './assets')
+    },
+    filename: function(req, file, cb){
+        cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`)
+    }
+})
+
+const getExt =(mimeType)=>{
+    switch(mimeType){
+        case "image/png":
+            return ".png";
+
+        case "image/jpeg":
+            return ".png";
+
+        case "image/jpg":
+            return ".jpg";
+                      
+    }
+}
+
+var assets = multer({storage: storage});
 
 
 app.get ('/api/posts', (req, res) =>{
@@ -28,7 +52,19 @@ app.get('/api/posts/:post_id', (req, res)=>{
     }
 });
 
+app.post('/api/posts', assets.single("post_image"), (req, res)=>{
+    console.log(req.body);
+    const newPost = {
+        "id":`${Date.now()}`,
+        "title": req.body.title,
+        "content": req.body.content,
+        "post_image": req.file.path,
+        "added_date": `${Date.now()}`
+    }
+    postData.add(newPost);
+    res.status(201).send(newPost);
 
+})
 
 
 app.listen(5000, console.log('listening to port 5000'));
